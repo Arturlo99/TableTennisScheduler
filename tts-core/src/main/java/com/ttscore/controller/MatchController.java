ce5f76163b4e56aa30eafd45d1b4814e0897b2be
Arturlo99 <arturac2@gmail.com>
2021-11-05 19:26:03 +0100

    [TTS-3] - Add test data, create MatchController

---
+++ tts-core/src/main/java/com/ttscore/controller/MatchController.java
@@ -0,0 +1,28 @@
+package com.ttscore.controller;
+
+import com.ttscore.model.Match;
+import com.ttscore.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/matches")
+@CrossOrigin(originPatterns = "http://localhost:4200")
+public class MatchController {
+    @Autowired
+    private final MatchRepository matchRepository;
+
+    public MatchController(MatchRepository matchRepository) {
+        this.matchRepository = matchRepository;
+    }
+
+    @GetMapping("/all")
+    List<Match> all() {
+        return matchRepository.findAll();
+    }
+}
