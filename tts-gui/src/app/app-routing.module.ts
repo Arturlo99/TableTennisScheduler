56f98205ede5b1dafbf16718f96046ca05b03eee
Arturlo99 <arturac2@gmail.com>
2021-11-11 15:51:25 +0100

    Create new component for tournaments tab

--- tts-gui/src/app/app-routing.module.ts
+++ tts-gui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BackgroundComponent } from './background/background.component';
+import { EventsListComponent } from './events-list/events-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BackgroundComponent },
+  { path: 'events/all', component: EventsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tts-gui/src/app/app.module.ts
+++ tts-gui/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BackgroundComponent } from './background/background.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EventsListComponent } from './events-list/events-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +24,8 @@ import { BackgroundComponent } from './background/background.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
