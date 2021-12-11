31ca29930b6ae9a8ee425a318ef8e24b9f745a9c
Arturlo99 <arturac2@gmail.com>
2021-12-11 22:14:25 +0100

    Add test cases for UserServiceImpl

--- tts-core/src/test/java/com/ttscore/UserServiceImplTest.java
+++ tts-core/src/test/java/com/ttscore/UserServiceImplTest.java
@@ -1,52 +1,129 @@
 package com.ttscore;
 
-import com.ttscore.controller.UserController;
+import com.ttscore.dto.CredentialsDTO;
+import com.ttscore.model.Role;
 import com.ttscore.model.User;
 import com.ttscore.repository.RoleRepository;
 import com.ttscore.repository.UserRepository;
 import com.ttscore.service.UserService;
-import org.junit.jupiter.api.BeforeAll;
+import com.ttscore.service.impl.UserServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import java.util.stream.Stream;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
-	@Mock
-	private UserRepository userRepository;
+    @Mock
+    private UserRepository userRepository;
 
-	@Mock
-	private RoleRepository roleRepository;
+    @Mock
+    private RoleRepository roleRepository;
 
-	@Mock
-	UserService userService;
+    @InjectMocks
+    UserServiceImpl userServiceImpl;
 
-	@InjectMocks
-	private UserController userController;
+    private static final String EMAIL = "EMAIL";
+    private static final String PASSWORD = "PASSWORD";
+    private static final String ROLE_USER = "ROLE_USER";
+    private static final String LOGGED_IN = "loggedIn";
+    private static final String TRUE = "true";
+    private static final String FALSE = "false";
+    private static final String ROLE = "role";
+    private static final Integer ZERO = 0;
+    private static final Integer ONE = 1;
+    private static final Integer ROLE_USER_ID = 2;
 
-	private static final String EMAIL = "EMAIL";
+    @Test
+    void shouldCreateNewUser() {
+        //given
+        Role role = new Role();
+        role.setName(ROLE_USER);
+        User user = new User();
+        user.setEmail(EMAIL);
+        when(userRepository.countUsingEmail(user.getEmail())).thenReturn(ZERO);
+        when(roleRepository.getById(ROLE_USER_ID)).thenReturn(role);
 
-	@Test
-	void shouldCreateNewUser() {
-		//given
-		User user = mock(User.class);
-		when(user.getEmail()).thenReturn(EMAIL);
+        //when
+        ResponseEntity<?> result = userServiceImpl.createNewUser(user);
 
-		//when
+        //then
+        Assertions.assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(user.getRole()).isEqualTo(role);
+        verify(userRepository).save(user);
+    }
 
-		//then
-	}
+    @Test
+    void shouldNotCreateNewUser() {
+        //given
+        User user = mock(User.class);
+        when(userRepository.countUsingEmail(user.getEmail())).thenReturn(ONE);
+
+        //when
+        ResponseEntity<?> result = userServiceImpl.createNewUser(user);
+
+        //then
+        Assertions.assertThat(result.getStatusCode()).isEqualTo(HttpStatus.NOT_ACCEPTABLE);
+        verify(userRepository, never()).save(user);
+    }
+
+    @Test
+    void shouldLogin() {
+        //given
+        CredentialsDTO credentialsDTO = mock(CredentialsDTO.class);
+        User user = mock(User.class);
+        Role role = mock(Role.class);
+        when(userRepository.findUserByEmail(credentialsDTO.getEmail())).thenReturn(user);
+        when(user.getPassword()).thenReturn(PASSWORD);
+        when(user.getRole()).thenReturn(role);
+        when(role.getName()).thenReturn(ROLE_USER);
+        when(credentialsDTO.getPassword()).thenReturn(PASSWORD);
+
+        //when
+        Map<String, String> result = userServiceImpl.login(credentialsDTO);
+
+        //then
+        Assertions.assertThat(result).containsOnly(Map.entry(LOGGED_IN, TRUE), Map.entry(ROLE, ROLE_USER));
+    }
+
+    @Test
+    void shouldNotLogin() {
+        //given
+        CredentialsDTO credentialsDTO = mock(CredentialsDTO.class);
+        User user = mock(User.class);
+        when(userRepository.findUserByEmail(credentialsDTO.getEmail())).thenReturn(user);
+        when(user.getPassword()).thenReturn(PASSWORD);
+        when(credentialsDTO.getPassword()).thenReturn(null);
+
+        //when
+        Map<String, String> result = userServiceImpl.login(credentialsDTO);
+
+        //then
+        Assertions.assertThat(result).containsOnly(Map.entry(LOGGED_IN, FALSE));
+    }
+
+    @Test
+    void shouldNotLoginAndReturnEmptyMap() {
+        //given
+        CredentialsDTO credentialsDTO = mock(CredentialsDTO.class);
+        when(userRepository.findUserByEmail(credentialsDTO.getEmail())).thenReturn(null);
+
+        //when
+        Map<String, String> result = userServiceImpl.login(credentialsDTO);
+
+        //then
+        Assertions.assertThat(result).isEmpty();
+    }
 }
